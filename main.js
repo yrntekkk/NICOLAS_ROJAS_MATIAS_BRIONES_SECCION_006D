0a4cf262da6cd884d4e7f092c009271fcea67e91
Matias Ignacio Briones Alegria <mati.brionesa@duocuc.cl>
2024-05-21 21:29:53 -0400

    Nueva animacion para el nombre de la pagina
    Borramos la otra porque no nos gustaba mucho e hicimos una que se ve mejor

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
+//Funcion para mostrar el Toast cada 10 segundos
 function mostrarNotificacion() {
     var toast = document.getElementById('liveToast');
     var bsToast = new bootstrap.Toast(toast);
     bsToast.show();
 }
-
 //Intervalo entre notificaciones 
 setInterval(mostrarNotificacion, 10000);
 
 
 
 
-window.onload = function() {
-    animarPuntos();
-};
+//Espera a que el DOM este cargado para ejecutar el codigo
+document.addEventListener("DOMContentLoaded", function(){
+    const enlace = document.querySelector("#navbar-brand"); //Para seleccionar el elemento por su id
+    const texto = '<span class="text-danger ms-2">Animal</span><span>Shop</span>'; //Texto que se anima
+    const velocidad = 150; //Velocidad a la que va la animacion
+    let contenido = '';  //Definimos esta variable para contener el texto animado
+    let indice = 0;
 
-
-
-function animarPuntos() {
-    var puntos = document.getElementById('puntos');
-    puntos.style.display = 'inline-block';
-}
+//Funcion para animar el texto
+    function animarTexto(){
+        //Este if es para ver si aun quedan caracteres que agregar al texto
+        if (indice < texto.length){
+            //Si es que aun faltan caracteres agrega el siguiente a la variable contenido
+            contenido += texto.charAt(indice);
+            //Cursor que da el efecto como si alguien estuviese escribiendo
+            enlace.innerHTML = contenido + '<span class="cursor"></span>';
+            indice++; //Se usa para pasar al siguiente caracter
+            setTimeout(animarTexto, velocidad); 
+        }
+        else{
+            enlace.innerHTML = contenido;
+        }
+    }
+    animarTexto();
+});
